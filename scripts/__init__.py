c1e710dd623a93aebd4d1e795865bb9b2c6610db
Kristina Kogan <164797261+TinaKgn@users.noreply.github.com>
2026-04-08 15:15:13 +0100

    Updated notebooks and scripts

---
+++ scripts/__init__.py
@@ -0,0 +1,11 @@
+"""
+Utility scripts for notebook workflows
+
+Available modules:
+-
+-
+-
+-
+"""
+
+__version__ = "1.0.0"
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="absa-tourism-reviews",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Utility scripts for notebook workflows",
+)
